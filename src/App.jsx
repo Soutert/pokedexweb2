efa5ebb417c667ad2a9d86d2b3c9b5bcd45109d3
Orlando Betancourth <orlando@uledger.io>
2025-07-17 20:05:35 -0600

    container and react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-import slideData from './store/carouselSlides';
-import Carousel from "./components/carousel/Carousel"
+import { Routes, Route, Link } from 'react-router';
+import Home from './pages/Home';
 import PokeList from "./components/pokelist/PokeList"
 
 function App() {
   return (
     <>
-      <h1>Pokedex</h1>
-      <Carousel
-       slides={slideData}/>
-      <PokeList />
+      <header>
+        <h1>Pokedex</h1>
+        <nav>
+          <ul>
+            <li><Link to="/">Carusel</Link></li>
+            <li><Link to="/pokelist">Pokedex</Link></li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <Routes>
+          <Route path='/' element={(<Home />)} />
+          <Route path="/pokelist" element={(<PokeList />)} />
+        </Routes>
+      </main>
+      <footer>
+        Copy 2025
+      </footer>
     </>
   )
 }
